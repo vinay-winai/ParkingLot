74ba84def25aa77a58b2560702e7539cc0aed733
nikhilesh <nik362k@gmail.com>
2023-01-19 16:00:37 +0530

    made changes to ticket class

--- src/main/java/org/vin/Ticket.java
+++ src/main/java/org/vin/Ticket.java
@@ -1,4 +1,14 @@
 package org.vin;
 
 public class Ticket {
+    public int ticketID = 1;
+    public String vehicleID;
+    public int entryTime = 0;
+    public int assignedLot;
+    public Ticket(){
+        this.ticketID = ticketID++;
+        this.vehicleID =
+                this.entryTime = entryTime++;
+        this.assignedLot = getParking();
+    }
 }
