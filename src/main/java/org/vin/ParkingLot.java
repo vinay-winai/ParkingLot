1280e606957d28088a1b6fae9399705059b75ee2
vinay-winai <vinai2k20@gmail.com>
2023-01-19 15:41:12 +0530

    ParkingLot added

--- src/main/java/org/vin/ParkingLot.java
+++ src/main/java/org/vin/ParkingLot.java
@@ -1,4 +1,19 @@
 package org.vin;
 
 public class ParkingLot {
+     private static final Boolean[] parkingLots = new Boolean[400];
+
+     int getParking(){
+         for (int i=0;i <400; i++){
+             if (parkingLots[i]) {
+                 parkingLots[i] = false;
+                 return i + 1;
+             }
+         }
+         return -1;
+     }
+
+     void setParking(int slot){
+         parkingLots[slot-1] = true;
+     }
 }
